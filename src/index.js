ead282b8c57d442bf77587e2d04d0415b14981e7
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-03-19 21:55:44 +0200

    show stacktrace on dev only

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import http from 'http';
 import cors from 'cors';
 import logger from 'morgan';
 import express from 'express';
-import { host, port, env } from 'c0nfig';
+import { host, port, env, mongo } from 'c0nfig';
 import v1 from './v1';
 
 const app = express();
@@ -15,5 +15,5 @@ app.use(cors());
 app.use('/v1', v1());
 
 http.createServer(app).listen(port, () => {
-    console.log(`api is listening on http://${host}:${port} env=${env}`);
+    console.log(`api is listening on http://${host}:${port} env=${env} db=${mongo.connection}`);
 });
--- src/v1/index.js
+++ src/v1/index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import fortune from 'fortune';
+import chalk from 'chalk';
 import jsonApiSerializer from 'fortune-json-api';
+import { env } from 'c0nfig';
 import store from './store';
+
+const listener = fortune.net.http(store, {
+    serializers: [
+        [jsonApiSerializer, {}]
+    ]
+});
 
 export default function () {
     const router = express.Router();
-    const listener = fortune.net.http(store, {
-        serializers: [
-            [jsonApiSerializer, {}]
-        ]
-    });
 
-    router.use(listener);
+    router.use((req, res) => {
+        return listener(req, res)
+            .catch(error => {
+                if ('development' === env) {
+                    console.log(chalk.red(error.stack));
+                }
+            });
+    });
 
     return router;
 }
